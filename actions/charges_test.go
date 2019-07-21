d5ad0344055af2a43c5a35d1ae555d6dd6402df6
eddie.chen <chenghung0510@gmail.com>
2019-07-21 20:43:32 +0800

    test(api): [POST /api/charges] mock stripe api call

--- actions/charges_test.go
+++ actions/charges_test.go
@@ -1,9 +1,25 @@
 package actions
 
+import (
+	"gopkg.in/h2non/gock.v1"
+)
+
 func (as *ActionSuite) Test_CreateChargeHandler() {
+	defer gock.Off()
+
 	reqBody := createChargeRequest{
 		AmountInCents: 9991,
 	}
+
+	defer gock.Off()
+
+	gock.New("https://api.stripe.com/").
+		Post("/v1/charges").
+		Reply(201).
+		JSON(map[string]interface{}{
+			"id":     "ch_xxxxxxxx",
+			"amount": 9991,
+		})
 
 	res := as.JSON("/charges").Post(reqBody)
 	resBody := createChargeResponse{}
